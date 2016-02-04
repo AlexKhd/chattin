0f5ec886b87498c036693bd38b429fd5c9c95e62
Alex Kh <khodos.alexey@gmail.com>
2016-02-03 23:10:32 -0300

    run mixpanel in test env

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,12 @@ class DashboardController < ApplicationController
   protected
 
   def identify_mixpanel_id
+    if Rails.env.production?
       tracker = Mixpanel::Tracker.new(Rails.application.secrets.mixpanel_key)
       tracker.track('ID', 'Script run')
+    elsif Rails.env.test?
+      tracker = Mixpanel::Tracker.new(ENV['MIXPANEL_KEY'])
+      tracker.track('ID', 'Script run')
+    end
   end
 end
