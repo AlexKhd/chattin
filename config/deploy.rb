0313b86dc982960be17b715b342b5662da053323
Alex Kh <khodos.alexey@gmail.com>
2016-03-09 01:31:01 -0300

    Add start websocket

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute "passenger-config restart-app --ignore-app-not-running /var/www/chatting_production/current"
+      execute "RAILS_ENV=production rake websocket_rails:start_server"
     end
   end
 
