0e427e90ef775efd7d99b21fbc57093dc7b9d390
Alex Kh <khodos.alexey@gmail.com>
2016-04-17 22:39:23 -0300

    Fix i18nIII via variable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require i18n/translations
 //= require bootstrap-sprockets
 //= require websocket_rails/main
 //= require_tree .
